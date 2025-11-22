8057a0495102b87bb94d2a981966f957170fe476
saramohebi <moheb.sara85@gmail.com>
2025-11-22 09:28:19 -0800

    fixing error

--- src/app/+public/public-template/public-template.ts
+++ src/app/+public/public-template/public-template.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
 
 
 @Component({
   selector: 'app-public-template',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLinkWithHref],
   templateUrl: './public-template.html',
   styleUrl: './public-template.scss',
 })
