5f06d38f808b9f104c55e8b277f4cf42cb43add1
saramohebi <moheb.sara85@gmail.com>
2025-11-22 13:13:30 -0800

    fixing

--- src/app/+public/loginpage/loginpage.ts
+++ src/app/+public/loginpage/loginpage.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-loginpage',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './loginpage.html',
   styleUrl: './loginpage.scss',
 })
 export class Loginpage {
+username:string='';
+password:string='';
+remember:boolean=false;
+router=inject(Router);
+message:string='';
+check(){
+  if (this.username=='admin' && this.password=='admin') {
+     this.router.navigateByUrl('/privatetemplate');
+  } else {
+    this.message='نام کاربری یا کلمه عبور صحیح نمیباشد!';
 
+  }
+}
 }
